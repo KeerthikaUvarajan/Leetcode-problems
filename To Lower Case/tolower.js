05815f98eb887abb4152cac1a0dde8e1ca5308be
Keerthika Uvarajan <keerthikauvarajan07@gmail.com>
2019-01-06 10:26:44 -0500

    Initial code

---
+++ To Lower Case/tolower.js
@@ -0,0 +1,20 @@
+/* PROBLEM: Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+
+ 
+
+Example 1:
+
+Input: "Hello"
+Output: "hello"
+Example 2:
+
+Input: "here"
+Output: "here"
+Example 3:
+
+Input: "LOVELY"
+Output: "lovely" */
+var toLowerCase = function(str) {
+    var lower = str.toLowerCase();
+    return lower;
+};
